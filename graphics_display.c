ebb2efdc9e7544694da0ac177c5614d2014c9a56
Pinakpani Mukherjee <mukherjee.pinakpani14@gmail.com>
2019-06-15 22:55:20 +0900

    added comments

--- graphics_display.c
+++ graphics_display.c
@@ -15,34 +15,34 @@ int main(int argc, char const *argv[])
 
 	SDL_Init(SDL_INIT_VIDEO);
 
-	window = SDL_CreateWindow("Game Window",
-							 SDL_WINDOWPOS_UNDEFINED,
-							 SDL_WINDOWPOS_UNDEFINED,
-							 640,
-							 480,
-							 0
+	window = SDL_CreateWindow("Game Window",          //name of game window
+							 SDL_WINDOWPOS_UNDEFINED, //initial x position
+							 SDL_WINDOWPOS_UNDEFINED, //initial y position
+							 1024,                    //width of window
+							 720,                     //height of window
+							 0                        //flags
 		                     );
 
 	renderer = SDL_CreateRenderer(window,-1,SDL_RENDERER_ACCELERATED);
 
 
 	SDL_SetRenderDrawColor(renderer,0,0,255,255);
-
+    //clears screen to blue
 	SDL_RenderClear(renderer);
-
+    //sets the draw color to white
 	SDL_SetRenderDrawColor(renderer,255,255,255,255);
 
-	SDL_Rect rect = {220,140,200,200};
-	SDL_RenderFillRect(renderer,&rect);
-
+	SDL_Rect rect = {220,140,200,200};  //positions of rectangle
+	SDL_RenderFillRect(renderer,&rect); //creates the rectangle
+    //presents the drawing,shows to the screen
 	SDL_RenderPresent(renderer);
+    //waits to quit after ~ miliseconds
+	SDL_Delay(2000);
 
-	SDL_Delay(10000);
+	SDL_DestroyWindow(window);    //destroys the window
+	SDL_DestroyRenderer(renderer);//destroys the renderer
 
-	SDL_DestroyWindow(window);
-	SDL_DestroyRenderer(renderer);
-
-	SDL_Quit();
+	SDL_Quit(); //quits the API
 
 
 	return 0;
